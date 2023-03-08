b71d01df023a9bf164ef9710de5beda795e05d35
Manuel Morales <mf.moraless1@uniandes.edu.co>
2023-03-08 13:04:44 -0300

    Add Manuel Class

---
+++ src/main/java/org/example/NombreManuel.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class NombreManuel {
+    public static void main(String[] args)
+    {
+        System.out.println("Mi Nombre es: " + "Manuel Morales");
+        System.out.println("Mi edad es: " + "27 Años");
+        System.out.println("Mi cargo es: " + "QA Engineer");
+    }
+}
