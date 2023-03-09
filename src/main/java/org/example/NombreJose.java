058319e6b64beade703ae2a9578687bc853c9746
JoseZambrano <jose.zambranograndes@crehana.com>
2023-03-09 10:51:34 -0300

    Commit de mi primera Clase en java

---
+++ src/main/java/org/example/NombreJose.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class NombreJose {
+    public static void main(String[] args)
+    {
+        System.out.println("Mi Nombre es: " + "Jose Zambrano");
+        System.out.println("Mi edad es: " + "24 Años");
+        System.out.println("Mi cargo es: " + "QA Engineer JR");
+    }
+}
