43f6e6422c633e2278ad8964b363f72def8ead86
Laura Garcia Pinzon <lauragarciapinzon@MacBook-Pro-de-Laura.local>
2023-03-13 15:52:03 -0500

    cambios lau

---
+++ src/main/java/org/example/Laura.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Laura {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Mi Nombre es: " + "Laura Garcia");
+        System.out.println("Mi edad es: " + "26 Años");
+        System.out.println("Mi cargo es: " + "Senior QA Engineer");
+    }
+
+}
