6abcc85a1d4510c5b4740f9b8e0b2a85875f4dae
Ana Mendoza <ana.mendoza@crehana.com>
2023-03-27 11:50:27 -0500

    Commit Ana

---
+++ src/main/java/org/example/AnaMendoza.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class AnaMendoza {
+
+    public static void main(String[] args){
+
+        System.out.println("Mi nombre es: Ana Mendoza");
+        System.out.println("Mi edad es: 32 años");
+        System.out.println("Mi cargo es: QA Engineer Semisenior");
+    }
+
+}
